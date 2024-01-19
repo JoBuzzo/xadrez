32aa3645971c421c3f935e90ea01db46c1ccd81b
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2024-01-19 00:06:24 -0300

    Movimentação da torre

---
+++ app/Services/Tower.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Services;
+
+class Tower
+{
+    public $abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+    public function __invoke($board, $position, $piece)
+    {
+        [$letter, $number] = str_split($position, 1);
+
+        $index = array_search($letter, $this->abc);
+
+        if($index == 0){
+            $index++;
+        }
+
+        if($index == 7){
+            $index--;
+        }
+        
+        $return = [];
+
+        if (Chess::PieceIsWhite($piece)) {
+            //verificar os espaços a esquerda apartir da posição da peça
+            for ($i = $index - 1; $i >= 0; $i--) {
+                if (isset($board[$this->abc[$i] . $number])) {
+                    if (Chess::PieceIsWhite($board[$this->abc[$i] . $number])) {
+                        break;
+                    }
+                    $return[] = $this->abc[$i] . $number;
+                    if (Chess::PieceIsBlack($board[$this->abc[$i] . $number])) {
+                        break;
+                    }
+                }
+            }
+
+
+            //verificar os espaços a direita apartir da posição da peça
+            for ($i = $index + 1; $i <= 7; $i++) {
+                if (isset($board[$this->abc[$i] . $number])) {
+                    if (Chess::PieceIsWhite($board[$this->abc[$i] . $number])) {
+                        break;
+                    }
+                    $return[] = $this->abc[$i] . $number;
+                    if (Chess::PieceIsBlack($board[$this->abc[$i] . $number])) {
+                        break;
+                    }
+                }
+            }
+
+            //verificar os espaços em baixo apartir da posição da peça
+            for ($i = $number - 1; $i >= 1; $i--) {
+                if (isset($board[$letter . $i])) {
+                    if (Chess::PieceIsWhite($board[$letter . $i])) {
+                        break;
+                    }
+                    $return[] = $letter . $i;
+                    if (Chess::PieceIsBlack($board[$letter . $i])) {
+                        break;
+                    }
+                }
+            }
+
+            //verificar os espaços em cima apartir da posição da peça
+            for ($i = $number + 1; $i <= 8; $i++) {
+                if (isset($board[$letter . $i])) {
+                    if (Chess::PieceIsWhite($board[$letter . $i])) {
+                        break;
+                    }
+                    $return[] = $letter . $i;
+                    if (Chess::PieceIsBlack($board[$letter . $i])) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (Chess::PieceIsBlack($piece)) {
+            //verificar os espaços a esquerda apartir da posição da peça
+            for ($i = $index + 1; $i >= 0; $i--) {
+                if (isset($board[$this->abc[$i] . $number])) {
+                    if (Chess::PieceIsBlack($board[$this->abc[$i] . $number])) {
+                        break;
+                    }
+                    $return[] = $this->abc[$i] . $number;
+                    if (Chess::PieceIsWhite($board[$this->abc[$i] . $number])) {
+                        break;
+                    }
+                }
+            }
+
+
+            //verificar os espaços a direita apartir da posição da peça
+            for ($i = $index + 1; $i <= 7; $i++) {
+                if (isset($board[$this->abc[$i] . $number])) {
+                    if (Chess::PieceIsBlack($board[$this->abc[$i] . $number])) {
+                        break;
+                    }
+                    $return[] = $this->abc[$i] . $number;
+                    if (Chess::PieceIsWhite($board[$this->abc[$i] . $number])) {
+                        break;
+                    }
+                }
+            }
+
+            //verificar os espaços em baixo apartir da posição da peça
+            for ($i = $number - 1; $i >= 1; $i--) {
+                if (isset($board[$letter . $i])) {
+                    if (Chess::PieceIsBlack($board[$letter . $i])) {
+                        break;
+                    }
+                    $return[] = $letter . $i;
+                    if (Chess::PieceIsWhite($board[$letter . $i])) {
+                        break;
+                    }
+                }
+            }
+
+            //verificar os espaços em cima apartir da posição da peça
+            for ($i = $number + 1; $i <= 8; $i++) {
+                if (isset($board[$letter . $i])) {
+                    if (Chess::PieceIsBlack($board[$letter . $i])) {
+                        break;
+                    }
+                    $return[] = $letter . $i;
+                    if (Chess::PieceIsWhite($board[$letter . $i])) {
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        return $return;
+    }
+}
--- app/Services/VerifyPiece.php
+++ app/Services/VerifyPiece.php
@@ -4,12 +4,16 @@ namespace App\Services;
 
 class VerifyPiece
 {
-
     public static function verify(array $board, $position, $piece)
     {
+
         switch ($piece) {
-            case 'peao_preta' || 'peao_branco':
+            case strstr($piece, 'peao'):
                 $result = new Pawn;
+                return $result($board, $position, $piece);
+                break;
+            case strstr($piece, 'torre'):
+                $result = new Tower;
                 return $result($board, $position, $piece);
                 break;
         }
