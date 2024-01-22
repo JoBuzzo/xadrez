9b1064807f63478cd0792760d7379dec690620db
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2024-01-22 11:30:58 -0300

    refatoração dos métodos da peça

--- app/Services/Piece.php
+++ app/Services/Piece.php
@@ -6,7 +6,7 @@ abstract class Piece
 {
     public static $abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
-    public static function possibleMoves($board, $position, $piece) : array
+    public static function possibleMoves($board, $position, $piece): array
     {
         return [];
     }
@@ -27,18 +27,16 @@ abstract class Piece
     }
 
 
-    //verifica se tem uma peça da mesma cor aonde na casa selecionada, caso não ter a peça é mexida
+    //verifica se tem uma peça da mesma cor na casa selecionada, caso não ter a peça é mexida
     public static function canCaptureOwnPiece($piece, $selectedPiece)
     {
-        return (!strstr($piece, 'branco') && strstr($selectedPiece, 'branco'))
-            || (!strstr($piece, 'preta') && strstr($selectedPiece, 'preta'));
+        return (!self::pieceIsWhite($piece) && self::pieceIsWhite($selectedPiece))
+            || (!self::pieceIsBlack($piece) && self::pieceIsBlack($selectedPiece));
     }
 
-    public static function piecesAreOfDifferentColors($piece, $secoundPiece)
+    public static function piecesAreOfDifferentColors($piece, $secondPiece)
     {
-        return (self::pieceIsWhite($piece) &&
-            self::pieceIsBlack($secoundPiece)
-            || self::pieceIsBlack($piece) &&
-            self::pieceIsWhite($secoundPiece));
+        return (self::pieceIsWhite($piece) && self::pieceIsBlack($secondPiece)
+            || self::pieceIsBlack($piece) && self::pieceIsWhite($secondPiece));
     }
 }
